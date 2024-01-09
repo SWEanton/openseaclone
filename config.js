e173778c0266bb7e5a5f18914ed3574db7396d26
Anton Henningsson <113432648+SWEanton@users.noreply.github.com>
2024-01-09 08:04:56 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+export const nftaddress = "0xe7DB92A4F83A8Cb86057e6775CFc88bF24757262";
+export const nftmarketaddress = "0xF330b54FaB0Fa5F9B5c799605E8e0496018ee84f";
+//EDIT !!!
+let x = 1;
+//change the above everytime you redeploy your contracts
+
+//command for deploying your contracts on hardhat:
+// npx hardhat run --network localhost scripts/deploy.js
+
+//command for starting or restarting your server:
+//npx hardhat node
